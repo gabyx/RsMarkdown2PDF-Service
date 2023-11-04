4fe01b9069bca8b6c589c5af118f6da497a55ad0
Silvio Heuberger <silvio.heuberger@gmail.com>
2023-11-04 11:07:06 -0400

    Add diesel as a dependency and dotenvy to read configuration from env

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,30 +2,20 @@
 extern crate rocket;
 use rocket::serde::{json::Json, Serialize};
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
-    format!(
-        "Hello, {} year old dude going by the name of {}!",
-        age, name
-    )
-}
 
 #[derive(Debug, Serialize)]
-struct Foo {
-    name: String,
-    message: String,
+struct Job {
+    id: String,
+    document_title: String,
 }
 
-#[get("/yo/<name>")]
-fn get_yo(name: String) -> Json<Foo> {
-    let foo = Foo {
-        name,
-        message: String::from("Yo is great!"),
-    };
-    Json(foo)
+#[get("/api/jobs")]
+fn get_all_jobs() -> Json<Vec<Job>> {
+    let result = vec![];
+    Json(result)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello, get_yo])
+    rocket::build().mount("/", routes![get_all_jobs])
 }
