b67fc63f3a04314546f9777d96cf79c0a4b71c6b
Gabriel Nützi <gnuetzi@gmail.com>
2024-01-01 14:41:24 +0100

    feat: Move handlers to separate module

--- components/api/src/main.rs
+++ components/api/src/main.rs
@@ -1,56 +1,13 @@
-use std::sync::Arc;
-
+use api::{handlers::install_handlers, state::AppState};
 use common::{
     config::get_env_var,
-    job::Job,
     log::{create_logger, info},
-    queue::{get_job_queue_config, setup_job_queue, JobQueue},
-    response::json,
+    queue::{get_job_queue_config, setup_job_queue},
 };
 
 use diesel::{pg::PgConnection, prelude::Connection};
 use dotenvy::dotenv;
-use rocket::{
-    config::{Config, LogLevel},
-    http::Status,
-    routes,
-    tokio::sync::Mutex,
-    State,
-};
-
-struct AppState {
-    log: Arc<slog::Logger>,
-
-    // TODO: Abstract away db connection, if possible: Make an interface in `common`
-    // such that only converter/api use the same interface and dont need to know if its postgres or
-    // something else.
-    db: Mutex<PgConnection>,
-    job_queue: JobQueue,
-}
-
-#[rocket::get("/api/jobs")]
-async fn get_all_jobs(state: &State<AppState>) -> json::JsonResponse<Vec<Job>> {
-    info!(state.log, "Handling 'get_all_jobs'...");
-    let result = vec![Job::new("new job")];
-
-    return json::success!(result);
-}
-
-#[rocket::post("/api/debug/publish-job")]
-async fn send_job(s: &State<AppState>) -> json::JsonResponse<Job> {
-    info!(s.log, "Publishing debug job into queue.");
-    let job = Job::new("new job");
-
-    return match s.job_queue.publish(&job).await {
-        Ok(_) => json::success!(job),
-        Err(e) => json::failure!(
-            Status::InternalServerError,
-            "Could not publish job id '{}', error: \n'{}'.",
-            job.id,
-            e
-        ),
-    };
-}
+use rocket::config::{Config, LogLevel};
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
@@ -68,23 +25,17 @@ async fn main() -> Result<(), rocket::Error> {
 
     let (creds, config) = get_job_queue_config();
     let job_queue = setup_job_queue(&log, creds, config).await;
+    let app_state = AppState::new(log.clone(), db_conn, job_queue);
 
     info!(log, "Start rocket.");
-    let app_state = AppState {
-        log: log.clone(),
-        db: Mutex::new(db_conn),
-        job_queue,
-    };
-
     let mut config = Config::from(Config::figment());
     config.log_level = LogLevel::Off;
 
-    rocket::custom(config)
+    let r = rocket::custom(config)
         .attach(common::rocket::LogFairing(log))
-        .mount("/", routes![get_all_jobs, send_job])
-        .manage(app_state)
-        .launch()
-        .await?;
+        .manage(app_state);
+
+    install_handlers(r).launch().await?;
 
     return Ok(());
 }
---
+++ components/api/src/state/mod.rs
@@ -0,0 +1,26 @@
+use common::queue::JobQueue;
+use diesel::pg::PgConnection;
+use rocket::tokio::sync::Mutex;
+use slog;
+use std::sync::Arc;
+
+pub struct AppState {
+    // TODO: Abstract away the log. Make a simple interface in `common`.
+    pub log: Arc<slog::Logger>,
+
+    // TODO: Abstract away db connection, if possible: Make an interface in `common`
+    // such that only converter/api use the same interface and dont need to know if its postgres or
+    // something else.
+    pub db: Mutex<PgConnection>,
+    pub job_queue: JobQueue,
+}
+
+impl AppState {
+    pub fn new(log: Arc<slog::Logger>, db_conn: PgConnection, job_queue: JobQueue) -> AppState {
+        return AppState {
+            log: log.clone(),
+            db: Mutex::new(db_conn),
+            job_queue,
+        };
+    }
+}
