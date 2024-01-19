dd97b44c948198da8191f4033de0b20772dccc11
Gabriel Nützi <gnuetzi@gmail.com>
2024-01-19 00:53:44 +0000

    feat: Move schema to `common`
    - Improve error handling. Use `snafu` library. Consolidate all errors in `common`.
      No need to have separate errors in each module in `common`.
      Make conversions to `response::Error`.
    - Move `schema` to `common::db` for access in `converter` to.

--- components/common/src/response/error.rs
+++ components/common/src/response/error.rs
@@ -1,26 +1,11 @@
-use core::fmt;
-use rocket::{http::Status, response::status::Custom};
-use std::backtrace;
+use rocket::response::status::Custom;
 
-#[derive(Debug)]
-pub struct Error {
-    inner: Custom<String>,
-}
+pub type Status = rocket::http::Status;
+pub struct Error(pub Custom<String>);
 
-/// We wrap the Custom error with our own Error to better denote what it is.
-/// Type alias behave shitty with Ctors...
 impl Error {
     pub fn new(status: Status, msg: String) -> Error {
-        return Error {
-            inner: Custom(status, msg),
-        };
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Customize so only `x` and `y` are denoted.
-        return write!(f, "{}", self.inner.1);
+        return Error(Custom(status, msg));
     }
 }
 
@@ -28,26 +13,8 @@ impl fmt::Display for Error {
 macro_rules! _error {
     ($status:expr, $($args:tt)+) => {
         $crate::response::error::Error::new($status, format!($($args)+))
+
     };
 }
 
 pub use _error as error;
-
-/// TODO: This is better handled probably with something similar to
-/// https://github.com/rust-lang-deprecated/error-chain.
-/// for now its a solution.
-impl From<std::io::Error> for Error {
-    fn from(value: std::io::Error) -> Self {
-        let b = backtrace::Backtrace::capture();
-        return error!(
-            Status::InternalServerError,
-            "IO Error: {}\nBacktrace:\n{}", value, b
-        );
-    }
-}
-
-impl From<Error> for Custom<String> {
-    fn from(value: Error) -> Self {
-        return value.inner;
-    }
-}
--- components/common/src/response/json.rs
+++ components/common/src/response/json.rs
@@ -1,9 +1,9 @@
+use crate::result;
 use rocket::{
     http::{Status, StatusClass},
+    response::status::Custom,
     serde::json::Json,
 };
-
-use rocket::response::status::Custom;
 
 /// A common JSON response which is a result containing either the `Ok` value
 /// `Json<R>`
@@ -24,13 +24,13 @@ pub fn new_failure<R>(status: Status, msg: String) -> JsonResponse<R> {
     return Err(Custom(status, msg));
 }
 
+/// Create a succeeded JSON response.
 #[macro_export]
 macro_rules! _success {
     ($data:expr) => {
         $crate::response::json::new_success($data)
     };
 }
-
 pub use _success as success;
 
 /// Create a failed JSON response and before doing so log it as error.
@@ -47,5 +47,41 @@ macro_rules! _failure {
         }
     };
 }
-
 pub use _failure as failure;
+
+impl From<result::Error> for Custom<String> {
+    fn from(value: result::Error) -> Self {
+        return super::Error(value.into()).into();
+    }
+}
+
+impl From<super::Error> for Custom<String> {
+    fn from(value: super::Error) -> Self {
+        return value.0;
+    }
+}
+
+impl From<result::Error> for super::Error {
+    fn from(value: result::Error) -> Self {
+        return match value {
+            result::Error::IOError {
+                source: _,
+                backtrace: _,
+            } => super::error!(Status::InternalServerError, "IO Error."),
+            result::Error::QueueError {
+                message: _,
+                source: _,
+                backtrace: _,
+            } => super::error!(Status::InternalServerError, "Queue Error."),
+            result::Error::DBError {
+                message: _,
+                source: _,
+                backtrace: _,
+            } => super::error!(Status::InternalServerError, "Database Error."),
+            result::Error::GenericError {
+                message: _,
+                source: _,
+            } => super::error!(Status::InternalServerError, "GenericError"),
+        };
+    }
+}
