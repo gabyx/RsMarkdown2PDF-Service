952502256565c8bc640a5807e86040253c733d3f
Gabriel Nützi <gnuetzi@gmail.com>
2024-04-24 14:17:30 +0200

    fix: Test

---
+++ tools/format-rust.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC1091
+set -e
+set -u
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+. "$ROOT_DIR/tools/general.sh"
+
+cd "$ROOT_DIR" &&
+    ci_container_mgr_run_mounted "$(pwd)" \
+        docker.io/gabyxgabyx/rsmd2pdf-service:ci-format-rust-2.0.1 \
+        cargo fmt -- --config-path /repo
