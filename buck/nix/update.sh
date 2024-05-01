a3beae92ca12fab432a35c98635149fa3b5ca479
Gabriel Nützi <gnuetzi@gmail.com>
2024-05-01 10:47:09 +0200

    feat: Add prelude of buck2 original

--- buck/nix/update.sh
+++ buck/nix/update.sh
@@ -6,7 +6,6 @@ set -o pipefail
 ROOT_DIR=$(git rev-parse --show-toplevel)
 . "$ROOT_DIR/tools/log.sh"
 
-# ------------------------------------------------------------------------------
 usage() {
     die "update.sh: perform various updates to Nix and Buck related data" \
         "Usage:" \
@@ -18,66 +17,14 @@ usage() {
         "  various combinations of" \
         "  the following steps, in the following given order:" \
         "" \
-        "    --flake|-f         Step 1: Update the flake.lock file." \
-        "    --buck2|-b         Step 2: Update buck2 nix expression" \
+        "    --flake|-f         Step 1: Update the 'flake.lock' file." \
+        "    --buck|-b          Step 2: Update buck nix expression" \
         "  Or, to do everything at once:" \
         "" \
         "    --all|-a           Run all steps in the above order"
 }
 
-# ------------------------------------------------------------------------------
-
-FLAKE=0
-BUCK2=0
-
-PARSED_ARGUMENTS=$(getopt -an update.sh -o hfba --long help,flake,buck2,all -- "$@")
-VALID_ARGUMENTS=$?
-[ "$VALID_ARGUMENTS" != "0" ] && usage
-
-eval set -- "$PARSED_ARGUMENTS"
-while :; do
-    case "$1" in
-    -h | --help) usage ;;
-    -f | --flake)
-        FLAKE=1
-        shift
-        ;;
-    -b | --buck2)
-        BUCK2=1
-        shift
-        ;;
-    -a | --all)
-        FLAKE=1
-        BUCK2=1
-        shift
-        ;;
-
-    --)
-        shift
-        break
-        ;;
-    *) echo "Unexpected option: $1 - this should not happen." && usage ;;
-    esac
-done
-
-[ "$FLAKE" = "0" ] && [ "$BUCK2" = "0" ] && usage
-
-print_info "Updating flake=$FLAKE, buck2=$BUCK2"
-# ------------------------------------------------------------------------------
-
-## Step 1: Update the flake.lock file.
-if [ "$FLAKE" = "1" ]; then
-    nix flake --accept-flake-config update "${ROOT_DIR}/buck/nix"
-fi
-
-# ------------------------------------------------------------------------------
-
-## Step 2: Update buck2 nix expression
-if [ "$BUCK2" = "1" ]; then
-    update_buck2
-fi
-
-function update_buck2() {
+function update_buck() {
     local t d r v i h p
 
     # update the hash, revision, and version
@@ -119,9 +66,57 @@ function update_buck2() {
     rm -r -f "$t"
 }
 
-# ------------------------------------------------------------------------------
+function parse_args() {
+    FLAKE=0
+    BUCK=0
+
+    local parsed_args
+    parsed_args=$(getopt -an update.sh -o hfba --long help,flake,buck,all -- "$@")
+
+    local valid_args=$?
+    [ "$valid_args" != "0" ] && usage
+
+    eval set -- "$parsed_args"
+    while :; do
+        case "$1" in
+        -h | --help) usage ;;
+        -f | --flake)
+            FLAKE=1
+            shift
+            ;;
+        -b | --buck2)
+            BUCK=1
+            shift
+            ;;
+        -a | --all)
+            FLAKE=1
+            BUCK=1
+            shift
+            ;;
+
+        --)
+            shift
+            break
+            ;;
+        *) echo "Unexpected option: $1 - this should not happen." && usage ;;
+        esac
+    done
+}
+
+parse_args "$@"
+
+[ "$FLAKE" = "0" ] && [ "$BUCK" = "0" ] && usage
+print_info "Updating flake=$FLAKE, buck=$BUCK"
+
+## Step 1: Update the flake.lock file.
+if [ "$FLAKE" = "1" ]; then
+    nix flake --accept-flake-config update "${ROOT_DIR}/buck/nix"
+fi
+
+## Step 2: Update buck2 nix expression
+if [ "$BUCK" = "1" ]; then
+    update_buck
+fi
 
 ## Step 3: Rebuild and push the cache
 # This got deleted, because we do not use it yet.
-
-# ------------------------------------------------------------------------------
--- docs/devops/docker/README.md
+++ docs/devops/docker/README.md
@@ -1,3 +1,45 @@
 # Nesting Containers with Podman
 
+<<<<<<< Updated upstream
 See [container-nesting](https://github.com/gabyx/container-nesting).
+||||||| Stash base
+Run
+
+```shell
+podman volume create podman-vol
+podman build -f Containerfile podman-test .
+
+# Start the first container.
+podman run --privileged --rm \
+    -v "podman-vol:/var/lib/containers" \
+    -it podman-test ./build.sh
+```
+
+to see that we can build `Containerfile` (`podman` engine) then execute the
+built container and inside call `./build.sh` again which recursively nests
+containers. Its just too cool that this works? 🤣
+
+Up to 3 nested containers it works until then something crashes when setting up
+new user namespaces. How to resolve it, no idea.
+=======
+Run
+
+```shell
+name=ttl.sh/podman-test
+podman build -f Containerfile -t "$name" --target original .
+podman push "$name"
+
+podman run --privileged --rm -it "$name" ./run.sh
+```
+
+to see that we can build `Containerfile` (`podman` engine) then execute the
+built container and inside call `./run.sh` again which recursively nests
+containers. You can also use
+`podman build -f Containerfile -t "$name" --target own .` to use an alpine
+podman image with the same issues.
+
+Its just too cool that this works? 🤣
+
+Up to 3 nested containers it works until then something crashes when setting up
+new user namespaces. How to resolve it, no idea.
+>>>>>>> Stashed changes
